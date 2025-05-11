e1d82333e6c612e811ac52e12e69db2a5dfbb331
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-05-12 00:05:00 +0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002')],
 
     'allowed_origins_patterns' => [],
 
